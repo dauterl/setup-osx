abdd8831c1c17f06b33c3934de5b9d64fde9a0fa
Luke Dauter <luke.dauter@gmail.com>
2019-11-01 08:32:53 -0700

    end with setup.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,12 +6,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-#Create directory structure
-
+#Create directory structure and get setup scripts from repo
 cd ~
-mkdir Development && cd Development
-mkdir Repositories && cd Repositories
+mkdir Development && mkdir Development/Repositories && mkdir Development/Repositories/setup-osx && cd Development/Repositories/setup-osx
+curl -#L https://github.com/dauterl/setup-osx/tarball/master | tar -xzv --strip-components 1 --exclude={README.md,LICENSE}
 
-cd Development/Repositories && curl -#L https://github.com/dauterl/setup-osx/tarball/master | tar -xzv --strip-components 1 --exclude={README.md,LICENSE}
-
-sh test.sh
+echo Ready to run setup.sh
