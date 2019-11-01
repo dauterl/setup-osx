5253e6797de1741973fe556efe6cb22010c8aad7
Luke Dauter <luke.dauter@gmail.com>
2019-11-01 08:15:27 -0700

    Create bootstrap.sh
    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!usr/bin/env bash
+
+#Get admin pw
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+#Create directory structure
+
+cd ~
+mkdir Development && cd Development
+mkdir Repositories && cd Repositories
+
+curl -#L https://github.com/dauterl/setup-osx/tarball/master | tar -xzv --strip-components 1 --exclude={README.md,LICENSE}
+
