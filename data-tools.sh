fd0d49ce14955cf65c702ab6f4c4c392c042cbe1
Luke Dauter <luke.dauter@gmail.com>
2019-11-03 11:53:22 -0800

    Update data-tools.sh

--- data-tools.sh
+++ data-tools.sh
@@ -57,45 +57,9 @@ export PIP_REQUIRE_VIRTUALENV=false
 pip install virtualenv
 pip install virtualenvwrapper
 
-echo "------------------------------"
-echo "Source virtualenvwrapper from ~/.extra"
-
-EXTRA_PATH=~/.extra
-echo $EXTRA_PATH
-echo "" >> $EXTRA_PATH
-echo "" >> $EXTRA_PATH
-echo "# Source virtualenvwrapper, added by pydata.sh" >> $EXTRA_PATH
-echo "export WORKON_HOME=~/.virtualenvs" >> $EXTRA_PATH
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> $EXTRA_PATH
-echo "" >> $BASH_PROFILE_PATH
-source $EXTRA_PATH
-
-###############################################################################
-# Python 2 Virtual Enviroment                                                 #
-###############################################################################
-
-echo "------------------------------"
-echo "Setting up py2-data virtual environment."
-
-# Create a Python2 data environment
-mkvirtualenv py2-data
-workon py2-data
-
-# Install Python data modules
-pip install numpy
-pip install scipy
-pip install matplotlib
-pip install pandas
-pip install sympy
-pip install nose
-pip install unittest2
-pip install seaborn
-pip install scikit-learn
-pip install "ipython[all]"
-pip install bokeh
-pip install Flask
-pip install sqlalchemy
-pip install mysql-python
+export WORKON_HOME=~/Envs
+mkdir -p $WORKON_HOME
+source /usr/local/bin/virtualenvwrapper.sh
 
 ###############################################################################
 # Python 3 Virtual Enviroment                                                 #
@@ -122,7 +86,6 @@ pip install "ipython[all]"
 pip install bokeh
 pip install Flask
 pip install sqlalchemy
-#pip install mysql-python  # Python 2 only, use mysqlclient instead
 pip install mysqlclient
 
 ###############################################################################
