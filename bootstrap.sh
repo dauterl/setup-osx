410e12e018a1a75f4fb4935a7bdd1f147e2a4d57
Luke Dauter <luke.dauter@gmail.com>
2024-09-05 08:37:49 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 #Create directory structure and get setup scripts from repo
-export LOCAL_REPO='Development/Repositories/setup-osx'
+export LOCAL_REPO='workspace/setup-osx'
 cd ~
 mkdir -p ${LOCAL_REPO} && cd ${LOCAL_REPO}
 curl -#L https://github.com/dauterl/setup-osx/tarball/master | tar -xzv --strip-components 1 --exclude={README.md,LICENSE}
