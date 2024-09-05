7722f3e182004e222257ca1987532286bf11169e
Luke Dauter <luke.dauter@gmail.com>
2024-09-05 10:27:12 -0700

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -6,7 +6,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/lukedauter/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/lukedauter/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 echo "------------------------------"
